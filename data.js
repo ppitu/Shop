5c1b5c80b14a20c26641c8284da7f5742acce639
ppitu <piotr.chmura.1998@o2.pl>
2020-10-16 19:58:20 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports =  function () {
+    return {
+        categories: ["Sporty wodne", "Piłka nożna", "Szachy"],
+        products: [
+            {id: 1, name: "Kajak", category: "Sporty wodne", description: "Jednoosobowa łódka", price: 990},
+            {id: 2, name: "Kamizelka ratunkowa", category: "Sporty wodne", description: "help", price: 100}
+        ],
+        order: []
+    }
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const jsonServer = require("json-server");
+const chokidar = require("chokidar");
+const cors = require("cors");
+
+const fileName = process.argv[2] || "./data.js";
+const port = process.argv[3] || 3500;
+
+let router = undefined;
+
+const app = express();
+
+const createServer = () => {
+    delete require.cache[require.resolve(fileName)];
+    setTimeout(() => {
+        router = jsonServer.router(fileName.endsWith(".js") ? require(fileName)() : fileName);
+    }, 100)
+}
+
+app.use(cors())
+app.use(jsonServer.bodyParser)
+app.use("/api", (req, resp, next) => router(req, resp, next));
+
+chokidar.watch(fileName).on("change", () => {
+    console.log("Ponowne wczytanie danych z usługi");
+    createServer();
+    console.log("Zakończono ");
+});
+
+app.listen(port, () => console.log("Usluga internetowa na porcie: ${port}"));
